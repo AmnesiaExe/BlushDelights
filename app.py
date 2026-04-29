51bde344ffaf165857e511a9b7191aecac16c072
Twisha <tjdomadia2006@outlook.com>
2026-04-29 22:13:20 +0800

    Initial commit - Flask cake order app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        amount = request.form['amount']
+        cake_type = request.form['cake_type']
+        flavour = request.form['flavour']
+        size = request.form['size']
+        occasion = request.form['occasion']
+        message = request.form['message']
+
+        print("New cake order received:")
+        print(name, email, amount, cake_type, flavour, size, occasion, message)
+
+        return render_template('success.html', name=name)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
