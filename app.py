0bef90df8b6c605e5c9d07c6fb9c12b910f65a8c
Twisha <tjdomadia2006@outlook.com>
2026-05-15 15:12:14 +0800

    cakes menu and images added

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Homepage
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Products page
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+# Individual product page
+@app.route("/product")
+def product_detail():
+    return render_template("product_detail.html")
+
+# Menu
+# Cakes Page
+@app.route("/cakes")
+def cakes():
+    return render_template("menu/cakes.html")
+
+# Tarts Page
+@app.route("/tarts")
+def tarts():
+    return render_template("menu/tarts.html")
+
+# Contact Page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Order page
+@app.route('/order')
+def order():
+    return render_template('order.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
